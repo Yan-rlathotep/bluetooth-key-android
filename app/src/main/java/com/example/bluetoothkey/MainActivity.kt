93b14175285f5d4af00bb039eac622d77437cd66
Yan-rlathotep <campo.yannis@gmail.com>
2019-11-05 16:03:11 +0100

    Create UI

--- app/src/main/java/com/example/bluetoothkey/MainActivity.kt
+++ app/src/main/java/com/example/bluetoothkey/MainActivity.kt
@@ -2,11 +2,18 @@ package com.example.bluetoothkey
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.content.Intent
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        btn_openMessenger.setOnClickListener {
+            startActivity(
+                Intent(this@MainActivity, MessengerClientActivity::class.java)
+            )
+        }
     }
 }
---
+++ app/src/main/java/com/example/bluetoothkey/MessengerClientActivity.kt
@@ -0,0 +1,21 @@
+package com.example.bluetoothkey
+
+import android.os.*
+import androidx.appcompat.app.AppCompatActivity
+
+
+class MessengerClientActivity: AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_messengerclient)
+    }
+
+    override fun onStart() {
+        super.onStart()
+    }
+
+    override fun onStop() {
+        super.onStop()
+    }
+}
