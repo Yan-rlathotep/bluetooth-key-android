54ebe6d04faa33da576ad9143973c2f8e364f09e
Yan-rlathotep <campo.yannis@gmail.com>
2019-11-05 16:39:29 +0100

    Handle start-stop-bind-unbind

--- app/src/main/java/com/example/bluetoothkey/MessengerClientActivity.kt
+++ app/src/main/java/com/example/bluetoothkey/MessengerClientActivity.kt
@@ -1,7 +1,11 @@
 package com.example.bluetoothkey
 
+import android.content.ComponentName
+import android.content.Intent
+import android.content.ServiceConnection
 import android.os.*
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_messengerclient.*
 
 
 class MessengerClientActivity: AppCompatActivity() {
@@ -9,13 +13,71 @@ class MessengerClientActivity: AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_messengerclient)
+        btn_startService.setOnClickListener {startService()}
+        btn_stopService.setOnClickListener {stopService()}
+        btn_bindToService.setOnClickListener {bindToService()}
+        btn_unbindFromService.setOnClickListener {unbindFromService()}
     }
 
-    override fun onStart() {
-        super.onStart()
+    companion object {
+        private const val SERVICE_PACKAGE_NAME = "com.example.bluetoothkey"
+        private const val SERVICE_CLASS_NAME =
+            "$SERVICE_PACKAGE_NAME.MessengerService"
+
+        val serviceIntent: Intent
+            get() {
+                return Intent()
+                    .setComponent(
+                        ComponentName(SERVICE_PACKAGE_NAME, SERVICE_CLASS_NAME)
+                    )
+            }
     }
 
-    override fun onStop() {
-        super.onStop()
+    private var serviceStarted: Boolean = false
+
+    private var boundToService: Boolean = false
+
+    private var serviceCallsMessenger: Messenger? = null
+
+
+    private fun startService() {
+        if (!serviceStarted) {
+           startService(serviceIntent)
+            serviceStarted = true
+        }
     }
+
+    private fun stopService() {
+        if (serviceStarted) {
+            stopService(serviceIntent)
+            serviceStarted = false
+        }
+    }
+
+    private fun bindToService() {
+        if (!boundToService) {
+            bindService(serviceIntent, messengerServiceConnection, BIND_AUTO_CREATE)
+        }
+    }
+
+    private fun unbindFromService() {
+        if (boundToService) {
+            unbindService(messengerServiceConnection)
+            boundToService = false
+            serviceCallsMessenger = null
+        }
+    }
+
+    private val messengerServiceConnection = object : ServiceConnection {
+        override fun onServiceDisconnected(name: ComponentName?) {
+            boundToService = false
+            serviceCallsMessenger = null
+        }
+
+        override fun onServiceConnected(name: ComponentName?, service: IBinder?) {
+            boundToService = true
+            serviceCallsMessenger = Messenger(service)
+        }
+    }
+
 }
